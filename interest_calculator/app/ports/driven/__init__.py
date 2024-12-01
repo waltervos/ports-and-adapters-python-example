5003d7220b65a297d58a0ee911f4c771a13ba390
Walter Vos <walter.vos@ns.nl>
2024-12-01 22:36:27 +0100

    Tests pass

---
+++ interest_calculator/app/ports/driven/__init__.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+from pendulum import Date
+
+
+class ForGettingTheInterestRate(ABC):
+    @abstractmethod
+    def get_rate(self) -> float: ...
+    
+class ForGettingTheCurrentDate:
+    @abstractmethod
+    def get_current_date(self) -> Date: ...
---
+++ interest_calculator/app/ports/driving/__init__.py
@@ -0,0 +1,22 @@
+from pendulum import Duration
+from interest_calculator.app.ports.driven import (
+    ForGettingTheCurrentDate,
+    ForGettingTheInterestRate,
+)
+
+
+class SimpleInterestCalculator:
+    def __init__(
+        self,
+        interest_rate_repo: ForGettingTheInterestRate,
+        current_date_repo: ForGettingTheCurrentDate,
+    ) -> None:
+        self._interest_rate_repo = interest_rate_repo
+        self._current_date_repo = current_date_repo
+
+    def interest_from_now(self, principal: int, period: Duration):
+        current_date = self._current_date_repo.get_current_date()
+        end_date = current_date + period
+        months = current_date.diff(end_date).in_months()
+        
+        return self._interest_rate_repo.get_rate() * months * principal
